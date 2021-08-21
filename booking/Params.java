338724ae3bfc017d497e4b1be58ecc6f6ddd518b
Atanas011 <atanas011@yahoo.com>
2021-08-21 16:05:32 +0200

    Rename Params.java to booking/Params.java

--- booking/Params.java
+++ booking/Params.java
@@ -7,7 +7,7 @@ public class Params {
 	protected static final String HOME_URL = "https://www.booking.com/";
 	protected static final String HOME_WHERE = "Paris";
 	protected static final String EXP_RES = "Booking.com : Hotels in Paris . Book your hotel now!";
-	protected static final String EXP_TARGET = "Novotel Paris Les Halles, Paris – Updated 2021 Prices";
+	protected static final String EXP_TARGET = "Novotel Paris Les Halles, Paris â€“ Updated 2021 Prices";
 	protected static final String RES_NAME = "Novotel Paris Les Halles";
 	protected static final int HOME_Y = 2021, HOME_M = 10, HOME_IN = 19, HOME_OUT = 25;
 }
